e4ef5a0ea55db1e99281f89b20dca0ef9408623c
Laênio Marques <105957017+laesant@users.noreply.github.com>
2023-11-16 11:23:36 -0300

    class model and provider

---
+++ lib/providers/great_places.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/models/place.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+
+  List<Place> get items => [..._items];
+
+  int get itemsCount => _items.length;
+
+  Place itemByIndex(int index) => _items[index];
+}
